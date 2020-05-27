cebbdc0dae54cafe05eb86dad794376715ec0389
LukaszW007 <boss@AsusROG.localdomain>
2020-05-27 08:55:10 +0200

    Update repository info

--- gatsby-react-bootstrap-starter/gatsby-config.js
+++ gatsby-react-bootstrap-starter/gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  pathPrefix: "/gatsby-react-bootstrap-starter",
+  pathPrefix: "/Ramboll-360MP-viewer",
   siteMetadata: {
-    title: `Gatsby React Bootstrap Starter`,
-    description: `A starter that includes react-bootstrap and react-icons, along with SASS compilation.`,
-    author: `Billy Jacoby`,
+    title: `Ramboll-360MP-viewer`,
+    description: `360-viewer of MP scans..`,
+    author: `Lukasz W`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
